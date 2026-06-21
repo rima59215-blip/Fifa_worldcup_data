01d89f21535846f76c3149e86fd222c3da3d599e
rima59215-blip <rima59215@gmail.com>
2026-06-21 13:07:33 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    // Add this line below to fix the subfolder asset routing paths on the server
+    base: '/Fifa_worldcup_data/',
 })
